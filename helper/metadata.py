13f192de99e5ea0c4ad95e1d0f48a9fd84a15010
kibigo! <marrus-sh@users.noreply.github.com>
2019-10-13 23:45:01 -0700

    Fix to helpers providing weird behaviour on missing metadata

--- helper/metadata.py
+++ helper/metadata.py
@@ -4,8 +4,8 @@ from . import content
 def text(doc, name, default=''):
 	return content.text(doc.get_metadata(name, builtin=False), doc) or default
 
-def inlines(doc, name, default=''):
+def inlines(doc, name, default=[]):
 	return content.inlines(doc.get_metadata(name, builtin=False), doc) or ([Str(default)] if isinstance(default, str) else default)
 
-def blocks(doc, name, default=None):
+def blocks(doc, name, default=[]):
 	return content.blocks(doc.get_metadata(name, builtin=False), doc) or ([Plain(Str(default))] if isinstance(default, str) else default)
